682b7c9cc18aaee20e4112c223d6a5e588a19d67
DaviBraz7 <davisouzabraz@gmail.com>
2023-06-14 18:10:06 -0300

    atualização dia 14/06/2023

---
+++ SQLQuery_blog2.sql
@@ -0,0 +1,44 @@
+CREATE TABLE [Role] (
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [Name] VARCHAR(80) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+
+    CONSTRAINT [PK_Role] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_Role_Slug] UNIQUE([Slug])
+)
+CREATE NONCLUSTERED INDEX [IX_Role_Slug] on [Role]([Slug])
+
+CREATE TABLE [UserRole](
+    [UserId] INT NOT NULL,
+    [RoleId] INT NOT NULL,
+
+    CONSTRAINT [PK_UserRole] PRIMARY KEY([UserId], [RoleId])
+)
+
+CREATE TABLE [Category](
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [Name] VARCHAR(80) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id]),
+    CONSTRAINT [UQ_Category_Slug] UNIQUE([Slug])
+)
+CREATE NONCLUSTERED INDEX [IX_Category_Slug] ON [Category]([Slug])
+
+CREATE TABLE [Post](
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [CategoryId] INT NOT NULL,
+    [AuthorId] INT NOT NULL,
+    [Title] VARCHAR(160) NOT NULL,
+    [Summary] VARCHAR(255) NOT NULL,
+    [Body] TEXT NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+    [CreateDate] DATETIME NOT NULL DEFAULT(GETDATE()),
+    [LastUpdateDate] DATETIME NOT NULL DEFAULT(GETDATE()),
+
+    CONSTRAINT [PK_Post] PRIMARY KEY([Id]),
+    CONSTRAINT [FK_Post_Category] FOREIGN KEY([CategoryId]) REFERENCES [Category]([Id]),
+    CONSTRAINT [FK_Post_Author] FOREIGN KEY([AuthorId]) REFERENCES [User]([Id]),
+    CONSTRAINT [UQ_Post_Slug] UNIQUE([Slug])
+)
+CREATE NONCLUSTERED INDEX [IX_Post_Slug] ON [Post]([Slug])
